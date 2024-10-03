d50dbbc5b27957ab690000401d069fd8e0440d8c
AryasatyKiranaa <143712447+AryasatyKiranaa@users.noreply.github.com>
2024-10-03 21:33:50 +0700

    Initial Commit

---
+++ Dashboard/dashboard.py
@@ -0,0 +1,113 @@
+
+import streamlit as st
+
+
+
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+# Load the dataset
+bikehour_df = pd.read_csv("data_terpakai.csv")
+
+# Sidebar untuk memilih rentang waktu tahun
+st.sidebar.title("Filter Rentang Tahun")
+year_range = st.sidebar.slider("Pilih Tahun", min_value=int(bikehour_df["yr"].min()), 
+                               max_value=int(bikehour_df["yr"].max()), value=(2011, 2012))
+
+# Hanya menggunakan satu metode untuk menampilkan gambar di sidebar
+st.sidebar.image('sepeda.png', width=250)
+
+# Filter berdasarkan tahun yang dipilih
+filtered_df = bikehour_df[(bikehour_df["yr"] >= year_range[0]) & (bikehour_df["yr"] <= year_range[1])]
+
+
+    
+
+# 1. Total Penyewaan Sepeda Berdasarkan Musim
+st.header("Perbandingan Penyewaan Sepeda Berdasarkan Musim")
+
+total_sewa_per_musim = filtered_df.groupby(["season", "yr"])["cnt"].sum().reset_index()
+
+fig, ax = plt.subplots()
+sns.barplot(data=total_sewa_per_musim, x="season", y="cnt", hue="yr", palette="magma", ax=ax)
+ax.set_ylabel("Total Jumlah Penyewaan")
+ax.set_title("Total Penyewaan Sepeda Berdasarkan Musim dan Tahun")
+plt.tight_layout()
+st.pyplot(fig)
+
+
+
+# 2. Total Penyewaan Sepeda Berdasarkan Cuaca
+st.header("Perbandingan Penyewaan Sepeda Berdasarkan Kondisi Cuaca")
+
+total_sewa_per_cuaca = filtered_df.groupby(["weathersit", "yr"])["cnt"].sum().reset_index()
+
+fig, ax = plt.subplots()
+sns.barplot(data=total_sewa_per_cuaca, x="weathersit", y="cnt", hue="yr", palette="magma", ax=ax)
+ax.set_ylabel("Total Jumlah Penyewaan")
+ax.set_title("Total Penyewaan Sepeda Berdasarkan Kondisi Cuaca")
+plt.tight_layout()
+st.pyplot(fig)
+
+# 3. Analisa Penyewaan Pada Tiap Hari
+st.header("Analisa Penyewaan Pada Tiap Hari")
+
+jumlah_rental_weekday = filtered_df.groupby(["weekday", "yr"])["cnt"].sum().reset_index()
+
+fig, ax = plt.subplots()
+sns.lineplot(data=jumlah_rental_weekday, x="weekday", y="cnt", hue="yr", palette="viridis", marker="o", ax=ax)
+ax.set_ylabel("Total Jumlah Penyewaan")
+ax.set_title("Analisa Penyewaan Pada Tiap Hari")
+plt.tight_layout()
+st.pyplot(fig)
+
+# 4. Perbandingan Pengguna Casual dan Terdaftar
+st.header("Perbandingan Pengguna Casual dan Terdaftar")
+
+casual_counts = filtered_df.groupby("yr")["casual"].sum().reset_index()
+casual_counts.columns = ["yr", "Total_Casual"]
+
+registered_counts = filtered_df.groupby("yr")["registered"].sum().reset_index()
+registered_counts.columns = ["yr", "Total_Registered"]
+
+fig, ax = plt.subplots()
+bar_width = 0.35
+index = casual_counts["yr"]
+
+ax.bar(index, casual_counts["Total_Casual"], bar_width, label="Casual Users", color="pink")
+ax.bar(index + bar_width, registered_counts["Total_Registered"], bar_width, label="Registered Users", color="purple")
+ax.set_xlabel("yr")
+ax.set_ylabel("Total Counts")
+ax.set_title("Casual vs Registered Users by Year")
+ax.legend()
+plt.tight_layout()
+st.pyplot(fig)
+
+# 5. Total Penyewaan Sepeda Berdasarkan Bulan
+st.header("Total Penyewaan Sepeda Berdasarkan Bulan")
+
+jumlah_rental_bulanan = filtered_df.groupby(["mnth", "yr"])["cnt"].sum().reset_index()
+
+fig, ax = plt.subplots()
+sns.lineplot(data=jumlah_rental_bulanan, x="mnth", y="cnt", hue="yr", palette="viridis", marker="o", ax=ax)
+ax.set_ylabel("Total Jumlah Penyewaan")
+ax.set_title("Total Penyewaan Sepeda Berdasarkan Bulan")
+plt.tight_layout()
+st.pyplot(fig)
+
+# 6. Total Penyewaan Sepeda Berdasarkan Jam
+st.header("Total Penyewaan Sepeda Berdasarkan Jam")
+
+jumlah_rental_jam = filtered_df.groupby(["hr", "yr"])["cnt"].sum().reset_index()
+
+fig, ax = plt.subplots()
+sns.lineplot(data=jumlah_rental_jam, x="hr", y="cnt", hue="yr", palette="viridis", marker="o", ax=ax)
+ax.set_ylabel("Total Jumlah Penyewaan")
+ax.set_xlabel("Jam")
+ax.set_title("Total Penyewaan Sepeda Berdasarkan Jam")
+plt.tight_layout()
+st.pyplot(fig)
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# Dashboard-Bicycle
+<<<<<<< HEAD
+# Dicoding-Bike-Sharing
+Penugasan dicoding dengan menggunakan bike sharing dataset
+=======
+# Bike Sharing Dashboard
+
+## Description
+
+Project ini berisikan proyek Analisis Data pada dataset Bike Sharing. Selanjutnya hasil analisis dibuat dashboard
+
+## Directory
+
+- `/dashboard`: Berisi file  yang ditampilkan untuk dashboard, 1 dataset, dan 1 gambar.
+- `/data` : Menyimpan dataset yang digunakan untuk analisis
+- `notebook.ipynb` : file jupyter notebook 
+- `README.md` : file informasi tentang proyek ini
+- `requirements.txt` : Berisikan library apa saja yang dilakukan 
+- `Video_Dashboard_Arya.mp4` : Video  yang menampilkan hasil dashboard 
+
+## Run steamlit app
+```
+streamlit run dashboard.py
+```
+
+## Streamlit Cloud 
+Link Streamlit Tirta: <a href='https://bikesharing-tirtaaj.streamlit.app/' target='_blank' title='Bike Sharing Dashboard by Tirta Agung Jati | Streamlit'>Bike Sharing Dashboard by Tirta Agung Jati</a>
+
+## Berikut Tampilan atau hasil screenshot dari dashboard
+
+![Screenshot Dashboard 1](SS-Dashboard-1.png)
+![Screenshot Dashboard 2](SS-Dashboard-2.png)
+![Screenshot Dashboard 3](SS-Dashboard-3.png)
+![Screenshot Dashboard 4](SS-Dashboard-4.png)
+![Screenshot Dashboard 5](SS-Dashboard-5.png)
+![Screenshot Dashboard 6](SS-Dashboard-6.png)
+>>>>>>> c8bb0ab (first commit)
